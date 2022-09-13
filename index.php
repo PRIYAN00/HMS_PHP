7996ced08f12164651771cd865b9f2d61caf6579
Priyanthan M <mahindarajpriyanthan@outlook.com>
2022-09-13 13:50:02 +0530

    first

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">Arogya Health Care - HMS</a>        
+            <a class="navbar-brand ps-3 fw-bolder text-uppercase" href="./">Arogya Health Care - HMS</a>        
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div class="input-group">
@@ -158,6 +158,12 @@
                     </div>
                 </nav>
             </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Dashboard</h1>
+                </div>
+            </main>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
