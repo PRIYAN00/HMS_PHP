698b120140f09978220f4b14833b1c5f91a6b32f
Priyanthan M <105900493+PRIYAN00@users.noreply.github.com>
2022-09-20 16:40:06 +0530

    Create log.php

---
+++ log.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+include('dbconn.php');
+if(isset($_SESSION['userName'])){ ?>
+
+
+// your html code
+// your html code
+// your html code
+// your html code
+// your html code
+// your html code
+
+<?ph
+p }else { ?>
+<script>
+alert('Please Login to Access HMS');
+	window.location="LoginForm.php";
+</script>
+<?php } ?>
