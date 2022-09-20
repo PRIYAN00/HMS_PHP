cd0b4380cab4b312a99dd18010fa4decb895f30b
Priyanthan M <105900493+PRIYAN00@users.noreply.github.com>
2022-09-20 16:43:57 +0530

    Create navbar2.php

---
+++ navbar2.php
@@ -0,0 +1,10 @@
+<nav class="sb-topnav navbar navbar-dark bg-dark">
+    <!-- Navbar Brand-->
+    <a class="navbar-brand ps-3 fw-bolder text-uppercase" href="./">Arogya Health Care - HMS</a>        
+    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+        <li class="nav-item dropdown">
+            <a class="text-uppercase text-white fw-bolder" style="text-decoration: none;"><?php echo $_SESSION['userName']; ?></a>
+        <a class="btn btn-danger" href="logout.php" >Logout</a>
+        
+    </ul>            
+</nav>
